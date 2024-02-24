c328ec38ab29dde92f7851db4da9811327ae7358
K PREM CHAND <113376768+Kpremchand123@users.noreply.github.com>
2024-02-24 12:36:00 +0530

    Add files via upload

---
+++ books.py
@@ -0,0 +1,44 @@
+# coding=System
+
+class account(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+
+
+
+# coding=System
+
+class books(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  kkkk  (private)
+
+  """
+
+  def lvda(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
